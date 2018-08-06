55eab34b761a77ff5538dfa58df9986aa8ebe986
github username <github email address>
2018-08-06 00:47:14 -0400

    middleware fix?

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let { ObjectID } = require("mongodb");
+let cors = require('cors');
 
 //include local files
 let { mongoose } = require("./db/mongoose");
@@ -12,15 +13,19 @@ let { Click } = require("./models/click");
 let app = express();
 const PORT = process.env.PORT || 3000;
 
-//configure middleware
-app.use(bodyParser.json()); //can now send JSON to express application
-
-//allow cross-origin access
+allow cross-origin access
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
+
+//configure middleware
+app.use(bodyParser.json()); //can now send JSON to express application
+
+// app.use(cors())
+
+
 
 //configure routes here
 app.post("/clicks", (request, response) => {
