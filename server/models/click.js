da4f6f4fb4186342deb999a04d5b8e54b5e56020
github username <github email address>
2018-08-06 00:21:35 -0400

    trying again

--- server/models/click.js
+++ server/models/click.js
@@ -3,11 +3,11 @@ let mongoose = require("mongoose");
 let Click = mongoose.model("Click", {
   //define schema here:
   xLocation: {
-    type: Number,
+    type: String,
     required: true,
   },
   yLocation: {
-    type: Number,
+    type: String,
     required: true,
   }
 });
