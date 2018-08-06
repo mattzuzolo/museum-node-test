28d6aa52a108cec5186ab69c57aa0ea548ca9794
github username <github email address>
2018-08-06 00:23:51 -0400

    trying again

--- server/models/click.js
+++ server/models/click.js
@@ -2,20 +2,14 @@ let mongoose = require("mongoose");
 
 let Click = mongoose.model("Click", {
   //define schema here:
-  text: {
-      type: String,
-      required: true,
-      minLength: 1,
-      trim: true,
-    },
-    completed: {
-      type: Boolean,
-      default: false
-    },
-    completedAt: {
-      type: Number,
-      default: null
-    }
+  xLocation: {
+    type: Number,
+    required: true
+  },
+  yLocation: {
+    type: Number,
+    required: true
+  }
 });
 
 module.exports = { Click };
