002d8e773d53c93cfb37f5c10fb6a97e679849b1
github username <github email address>
2018-08-06 00:02:39 -0400

    fixed schema problem

--- server/models/click.js
+++ server/models/click.js
@@ -4,12 +4,10 @@ let Click = mongoose.model("Click", {
   //define schema here:
   xLocation: {
     coord: Number,
-    default: -1
   },
   yLocation: {
     coord: Number,
-    default: -1
-  },
+  }
 });
 
 module.exports = { Click };
